0a5e5d84d02061cf806092a59b032f44ab05dcd2
Joao Gabriel <60220208+JoaoGabrielMenezes@users.noreply.github.com>
2021-06-12 15:49:37 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,37 @@
 import java.security.SecureRandom;
 import java.util.Scanner;
-
-import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
+import java.awt.datatransfer.StringSelection;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 /**
  * @author Joao Menezes
  */
 public class Main {   
-    private static int lim,randomIndex,pass;
-    final private static String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\\/+=-_()[]*&%$#@!";
+    private static int lim,randomIndex,option;
+    final private static String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\\/+=-_()[]*&%$#@!.;,?";
+    private static String certeza;
     private static Scanner sc;
     private static SecureRandom random;
     private static StringBuilder builder;
     private static StringSelection stringSelection;
     private static Clipboard clipboard;
+    private static final String ANSI_RESET = "\u001B[0m", ANSI_RED = "\u001B[31m", ANSI_GREEN = "\u001B[32m";
     
     public static void clear() throws InterruptedException, IOException {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }
+
+    public static void confirmar() throws InterruptedException, IOException {
+            System.out.println("Certeza que deseja sair? (y/n)");
+            certeza = sc.next();
+        if (certeza.toLowerCase().equals("s") || certeza.toLowerCase().equals("y")) {
+            Thread.sleep(1000);
+            clear();
+            System.exit(0); 
+        }else{
+            opcoes();
+        }
     }
 
     public static String generatePassword(){
@@ -37,45 +50,38 @@ public class Main {
  
     public static void copy() throws InterruptedException, IOException {
         //Copia para area de transferencia
-        //seleciona a String
         stringSelection = new StringSelection(builder.toString());
-        //adiciona a var clipboard um sistema para copiar
         clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        //atribui a essa var a string desejada a ser copiada
         clipboard.setContents(stringSelection, null);
         /***/
-        System.out.println("Copiado para area de transferencia");
+        System.out.println(ANSI_GREEN+"Copiado"+ANSI_RESET);
         Thread.sleep(1000);
         clear();
-        lim = 0;
-        _init_();
+        Thread.sleep(550);
+        System.exit(0);
     }
 
     public static void opcoes() throws InterruptedException, IOException
     {
          try {
-            System.out.println("\n[1] - Copiar para area de transferencia?\n[2] - Repetir senha\n[3] - Repetir gerador\n[4] - Sair ");
-            pass = sc.nextInt();
-            if (pass == 1) {
+            System.out.println("\n[1] - Copiar senha?\n[2] - Repetir senha\n[3] - Repetir gerador\n[4] - Sair ");
+            option = sc.nextInt();
+            if (option == 1) {
                 copy();
-            }else if(pass == 2){
+            }else if(option == 2){
                 clear();
-                System.out.println("Password: "+generatePassword()); 
-            }else if(pass == 3){
+            }else if(option == 3){
                 Thread.sleep(500);
                 clear();
-                lim = 0;
                 _init_();
-            }else if(pass == 4){
-                Thread.sleep(1000);
-                clear();
-                System.exit(0);
+            }else if(option == 4){
+                confirmar();
             }
 
-            while (pass != 1 || pass != 2 || pass != 3) {
+            while (option != 1 || option != 2 || option != 3 || option != 4) {
                 clear();
-                if (pass == 2) {
-                    System.out.println("Password: "+generatePassword()); 
+                if (option == 2) {
+                   System.out.println("Password: "+ANSI_GREEN+generatePassword()+ANSI_RESET); 
                 }
                 opcoes();
             }
@@ -83,16 +89,17 @@ public class Main {
     }
 
     public static void _init_() throws InterruptedException, IOException {
+        lim = 0;
         sc = new Scanner(System.in);
         try {
             while(lim <= 0){
                 System.out.println("Quantos caracteres serao inseridos? ");
                 lim = sc.nextInt();
             }
-            System.out.println("Password: "+generatePassword());  
+            System.out.println("Password: "+ANSI_GREEN+generatePassword()+ANSI_RESET);  
             opcoes(); 
         } catch (Exception e) {
-           System.err.println("Digite apenas numeros inteiros");
+           System.err.println(ANSI_RED+"Digite apenas numeros inteiros"+ANSI_RESET);
            Thread.sleep(1000);
            _init_();
         }
